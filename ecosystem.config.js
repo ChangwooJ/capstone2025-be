d2496f13600ada57febef0239c49572fc19b7098
ChangwooJ <wjdckddn99@naver.com>
2025-05-20 15:15:49 +0900

    Fix: mysql db 재연결 로직 추가

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,10 @@ module.exports = {
         script: "./server.js",
         env: {
           NODE_ENV: "production",
-          // 필요시 다른 환경변수도 추가
+          JWT_SECRET : "4c0d608098b78d61cf5654965dab8b53632bf831dc6b43f29289411376ac107b",
+          UPBIT_OPEN_API_ACCESS_KEY : "sGzGpFhhxYe2JBFt5RXcGWod2WIczd9pkjlqRxFV",
+          UPBIT_OPEN_API_SECRET_KEY : "hyJRCyR4SZK3xk1mDZIrn995zkSbIbslkdOJWKhD",
+          DB_PASSWORD : "nexbit2025!"
         },
         watch: false, // 코드 변경 시 자동 재시작을 원하면 true로 설정
       },
--- handlers/DBinfo.js
+++ handlers/DBinfo.js
@@ -1,29 +1,15 @@
-const mysql = require('mysql2');
+import mysql from 'mysql2';
 require('dotenv').config();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: 'capstone2025.cn4owaaswe8j.eu-north-1.rds.amazonaws.com',
   user: 'admin',
   password: process.env.DB_PASSWORD,
   database: 'capstone2025',
-  port: 3306
-});
-
-
-// const db = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: '1111',
-//   database: 'capstone2025',
-//   port: 3306
-// });
-
-db.connect(err => {
-    if (err) {
-        console.error('Error connecting to database:', err);
-        return;
-    }
-    console.log('Connected to database');
+  port: 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 module.exports = db;
