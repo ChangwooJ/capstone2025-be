a911390e56b0e8201d1447f53cc60c47cca7c67d
ChangwooJ <wjdckddn99@naver.com>
2025-05-21 14:18:31 +0900

    Fix: 알고리즘 서버 엔드포인트 변경

--- handlers/userAlgorismApi.js
+++ handlers/userAlgorismApi.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const getTradingLog = async (req, res) => {
     try {
-        const response = await axios.get('http://13.211.209.54:8000/trade_log');
+        const response = await axios.get('http://13.211.77.105:8000/trade_log');
         res.status(200).json(response.data);
         console.log(response);
     } catch (error) {
@@ -17,7 +17,7 @@ const getTradingLog = async (req, res) => {
 
 const getAlgorithmLogs = async (req, res) => {
     try {
-        const response = await axios.get('http://13.211.209.54:8000/logs');
+        const response = await axios.get('http://13.211.77.105:8000/logs');
         res.status(200).json(response.data);
     } catch (error) {
         console.error('알고리즘 로그 조회 중 오류 발생:', error);
@@ -30,7 +30,7 @@ const getAlgorithmLogs = async (req, res) => {
 
 const startAiTrading = async (req, res) => {
     try {
-        const response = await axios.post('http://13.211.209.54:8000/start');
+        const response = await axios.post('http://13.211.77.105:8000/start');
         res.status(200).json(response.data);
         console.log("거래 시작");
     } catch (error) {
@@ -44,7 +44,7 @@ const startAiTrading = async (req, res) => {
 
 const stopAiTrading = async (req, res) => {
     try {
-        const response = await axios.post('http://13.211.209.54:8000/stop');
+        const response = await axios.post('http://13.211.77.105:8000/stop');
         res.status(200).json(response.data);
         console.log("거래 중지");
     } catch (error) {
@@ -58,7 +58,7 @@ const stopAiTrading = async (req, res) => {
 
 const getAiStatus = async (req, res) => {
     try {
-        const response = await axios.get('http://13.211.209.54:8000/status');
+        const response = await axios.get('http://13.211.77.105:8000/status');
         res.status(200).json(response.data);
     } catch (error) {
         console.error('알고리즘 로그 조회 중 오류 발생:', error);
